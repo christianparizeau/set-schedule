ae3deee4a47ba0ff93e2eb8a71425dc2173c26b5
Christian <christianparizeau@gmail.com>
2020-03-30 14:53:10 -0700

    fixed react-dom import in index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,5 @@
 import App from './components/app';
-import { React, ReactDOM } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
